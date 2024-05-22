74c5d23b5ecdc646f7040a27a46adfe5464d74f8
HypnosCicero <1345764659@qq.com>
2024-05-22 08:02:05 +0800

    feat(test):add a test case

--- lib/res/color_scheme.dart
+++ lib/res/color_scheme.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
+
 ColorScheme tomatoColorScheme = ColorScheme.fromSeed(seedColor: Colors.red);
 ColorScheme backgroundColorScheme = ColorScheme.fromSeed(seedColor: Colors.white);
 ColorScheme leavesColorScheme = ColorScheme.fromSeed(seedColor: Colors.green);
+
+class Checkout {
+  void printColor() {
+    Color tomatoPrimaryColor = tomatoColorScheme.primary;
+    Color tomatoOnPrimaryColor = tomatoColorScheme.onPrimary;
+    Color tomatoInversePrimary = tomatoColorScheme.inversePrimary;
+    print("tomatoColorScheme.primary = $tomatoPrimaryColor");
+    print("tomatoColorScheme.onPrimary = $tomatoOnPrimaryColor");
+    print("tomatoColorScheme.inversePrimary = $tomatoInversePrimary");
+
+    Color leavesPrimaryColor = leavesColorScheme.primary;
+    Color leavesOnPrimaryColor = leavesColorScheme.onPrimary;
+    print("leavesColorScheme.primary = $leavesPrimaryColor");
+    print("leavesColorScheme.onPrimary = $leavesOnPrimaryColor");
+  }
+}
---
+++ test/color_scheme_test.dart
@@ -0,0 +1,4 @@
+import 'package:just_a_pomodoro/res/color_scheme.dart';
+void main() {
+  Checkout().printColor();
+}
