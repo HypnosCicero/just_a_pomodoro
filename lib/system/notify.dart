e260f010dc745691be7e2d796f5b4d619104d375
HypnosCicero <1345764659@qq.com>
2024-05-26 20:46:39 +0800

    feat(s/notify):standardized code structure.

--- lib/system/notify.dart
+++ lib/system/notify.dart
@@ -1,33 +1,53 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-void showNotification() async {
-  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-    'channel_id',
-    'channel_name',
-    'channel_description',
-    importance: Importance.max,
-    priority: Priority.high,
-    //sound: RawResourceAndroidNotificationSound('your_custom_sound'),
-  );
-
-  var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-
-  var macOSPlatformChannelSpecifics = MacOSNotificationDetails();
+class Notify {
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
   
-  var platformChannelSpecifics = NotificationDetails(
-    android: androidPlatformChannelSpecifics,
-    iOS: iOSPlatformChannelSpecifics,
-    macOS: macOSPlatformChannelSpecifics,
-  );
+  Notify() {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    initializeNotifications();
+  }
+  Future<void> initializeNotifications() async {
+    var initializationSettings = InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      iOS: IOSInitializationSettings(),
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: onSelectNotification,
+    );
+  }
 
-  await flutterLocalNotificationsPlugin.show(
-    0,
-    'Is Time to Rest',
-    'You are hard work by 25 minutes',
-    platformChannelSpecifics,
-    payload: 'notification_payload',
-  );
+  Future<void> onSelectNotification(String? payload) async {
+    // Handle notification selection here
+  }
+
+  void showNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      'channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      //sound: RawResourceAndroidNotificationSound('your_custom_sound'),
+    );
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+
+    var macOSPlatformChannelSpecifics = MacOSNotificationDetails();
+
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics,
+      macOS: macOSPlatformChannelSpecifics,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Is Time to Rest',
+      'You are hard work by 25 minutes',
+      platformChannelSpecifics,
+      payload: 'notification_payload',
+    );
+  }
 }
