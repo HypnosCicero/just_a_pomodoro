eba40988ddedf6291d94d67faf332f271b5c0780
HypnosCicero <1345764659@qq.com>
2024-05-26 20:48:49 +0800

    fix(test):fix the bug in notify_test

--- test/notify_test.dart
+++ test/notify_test.dart
@@ -1,5 +1,7 @@
-import 'package:just_a_pomodoro/system/notify.dart';
+//import 'package:just_a_pomodoro/system/notify.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:just_a_pomodoro/system/client/notify.dart';
 
 void main () {
-  showNotification(); 
+  test("description",() => showNotification());
 }
