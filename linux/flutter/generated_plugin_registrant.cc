4cd8f4206be21f2df546bcc0b959dcd4142e0686
HypnosCicero <1345764659@qq.com>
2024-05-27 21:22:02 +0800

    fix(about c):who can fix my  code bugs.

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -4,8 +4,12 @@
 
 // clang-format off
 
-#include "generated_plugin_registrant.h"
+//#include "generated_plugin_registrant.h"
 
+#include <awesome_notifications/awesome_notifications_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) awesome_notifications_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "AwesomeNotificationsPlugin");
+  awesome_notifications_plugin_register_with_registrar(awesome_notifications_registrar);
 }
