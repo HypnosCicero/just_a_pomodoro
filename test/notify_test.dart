e419f9018734c82e54da5a996a1586855f43272e
HypnosCicero <1345764659@qq.com>
2024-05-28 22:03:19 +0800

    fix(test):cleaned up previous code.

--- test/notify_test.dart
+++ test/notify_test.dart
@@ -1,7 +1,5 @@
 //import 'package:just_a_pomodoro/system/notify.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:just_a_pomodoro/system/client/notify.dart';
 
 void main () {
-  test("description",() => showNotification());
 }
