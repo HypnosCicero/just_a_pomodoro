c8f7664ace9f9a16c981bb1f18f94e947b723b0a
HypnosCicero <1345764659@qq.com>
2024-06-09 14:07:07 +0800

    feat(main):init flutter_local_notifications.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:just_a_pomodoro/home/pomodoro_app.dart';
+import 'package:just_a_pomodoro/view/pomodoro_app.dart';
+import 'package:just_a_pomodoro/system/notify.dart';
 
 void main() {
+  Notify.initNotification();
   runApp(const PomodoroApp());
 }
